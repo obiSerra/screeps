8476df760423cd7700981310d99cff624bcc7d4d
obione <obi.serra@gmail.com>
2026-03-04 00:22:42 +0100

    build roads

--- role.harvester.js
+++ role.harvester.js
@@ -14,8 +14,10 @@ var roleHarvester = {
         filter: (structure) => {
           return (
             (structure.structureType == STRUCTURE_EXTENSION ||
+              structure.structureType == STRUCTURE_ROAD ||
               structure.structureType == STRUCTURE_SPAWN ||
-              structure.structureType == STRUCTURE_TOWER) &&
+              structure.structureType == STRUCTURE_TOWER
+            ) &&
             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
           );
         },
