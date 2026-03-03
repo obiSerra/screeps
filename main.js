2067bcffdc5384063a57a8a9e61a94c8718959ff
Roberto Serra <roberto.serra@waterview.it>
2026-03-03 15:06:26 +0100

    room_config

--- main.js
+++ main.js
@@ -1,65 +1,64 @@
-const roleHarvester = require('role.harvester');
-const roleUpgrader = require('role.upgrader');
-const roleBuilder = require('role.builder');
+const roleHarvester = require("role.harvester");
+const roleUpgrader = require("role.upgrader");
+const roleBuilder = require("role.builder");
 
-const utils = require('utils');
+const utils = require("utils");
+const room_config = require("room_config");
 
 module.exports.loop = function () {
 
-const base_name = 'FirstBase'
-const roster = {
-    'harvester': 2,
-    'builder': 0,
-    'upgrader': 0
-}
+  function spawnProcedure() {
+    Object.keys(roster).forEach((role) => {
+      const creeps = _.filter(
+        Game.creeps,
+        (creep) => creep.memory.role == role,
+      );
+      // console.log(`${role}s: ${creeps.length}`);
 
-
-
-
-function spawnProcedure() {
-    Object.keys(roster).forEach(role => {
-        const creeps = _.filter(Game.creeps, (creep) => creep.memory.role == role);
-        // console.log(`${role}s: ${creeps.length}`);
-
-        if (creeps.length < roster[role]) {
-            const newName = `${role.charAt(0).toUpperCase() + role.slice(1)}${Game.time}`;
-            // console.log(`Spawning new ${role}: ${newName}`);
-            Game.spawns[base_name].spawnCreep([WORK, CARRY, MOVE], newName, { memory: { role: role } });
-        }
+      if (creeps.length < roster[role]) {
+        const newName = `${role.charAt(0).toUpperCase() + role.slice(1)}${Game.time}`;
+        // console.log(`Spawning new ${role}: ${newName}`);
+        Game.spawns[base_name].spawnCreep([WORK, CARRY, MOVE], newName, {
+          memory: { role: role },
+        });
+      }
     });
+  }
 
-}
+  for (var name in Memory.creeps) {
+    if (!Game.creeps[name]) {
+      delete Memory.creeps[name];
+      // console.log('Clearing non-existing creep memory:', name);
+    }
+  }
 
+  spawnProcedure();
 
-for(var name in Memory.creeps) {
-        if(!Game.creeps[name]) {
-            delete Memory.creeps[name];
-            // console.log('Clearing non-existing creep memory:', name);
-        }
+  Object.keys(Game.rooms).forEach((roomName) => {
+    const room = Game.rooms[roomName];
+    const config = room_config[roomName];
+
+    if (Game.spawns[base_name].spawning) {
+      var spawningCreep = Game.creeps[Game.spawns["Spawn1"].spawning.name];
+      Game.spawns[base_name].room.visual.text(
+        "🛠️" + spawningCreep.memory.role,
+        Game.spawns[base_name].pos.x + 1,
+        Game.spawns[base_name].pos.y,
+        { align: "left", opacity: 0.8 },
+      );
     }
 
-    spawnProcedure();
-
-    if(Game.spawns[base_name].spawning) {
-        var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
-        Game.spawns[base_name].room.visual.text(
-            '🛠️' + spawningCreep.memory.role,
-            Game.spawns[base_name].pos.x + 1,
-            Game.spawns[base_name].pos.y,
-            {align: 'left', opacity: 0.8});
+    for (var name in Game.creeps) {
+      var creep = Game.creeps[name];
+      if (creep.memory.role == "harvester") {
+        roleHarvester.run(creep);
+      }
+      if (creep.memory.role == "upgrader") {
+        roleUpgrader.run(creep);
+      }
+      if (creep.memory.role == "builder") {
+        roleBuilder.run(creep);
+      }
     }
-
-
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        }
-    }
-}
+  });
+};
